d3f4ab1935e0098e4f0707f12db2d238f2884871
David Parent <david.parent@sjsu.edu>
2024-11-01 16:01:24 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,26 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+module tt_um_QIF_8bit #( parameter MAX_COUNT = 24'd10_000_000 ) (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input vector b
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the output of membrane
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path Not used
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path Not used
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    assign uio_oe = 8'b00000001;
+    //assign uio_out[7:0] = 8'd0;
+    assign uio_out = 0;
+    assign uio_oe  = 0;
+   
+     QIFNeuron neuron (
+       .clk(clk),
+       .rst_n(rst_n),
+       .I_syn(ui_in),
+       .V_mem(uo_out)
+     );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
